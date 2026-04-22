809675136dc378784497e2e9c3054a17f2ccd1ba
mittalmukul1197 <mittalmukul1197@gmail.com>
2026-04-22 11:47:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,3 +3,37 @@ function toggleMenu() {
     document.getElementById("sidebar").classList.toggle("active");
 }
 
+
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+window.addEventListener("load", () => {
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+  }
+});
+
+
+
+function toggleDarkLight() {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+window.addEventListener("load", () => {
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+  }
+});
